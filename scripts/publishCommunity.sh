12ff2f5d1398ec238f55aaa23e8cadc1cf6e86d5
Simon Cook <simon.cook@salesforce.com>
2021-01-26 09:31:21 +0000

    added community delays to create scripts

--- scripts/publishCommunity.sh
+++ scripts/publishCommunity.sh
@@ -1,2 +1,7 @@
 #!/bin/zsh
 sfdx force:community:publish --name "GDS Demo"
+echo "Waiting for community to publish ..."
+sleep 20
+sfdx force:community:publish --name "GDS Landing"
+echo "Waiting for community to publish ..."
+sleep 20
