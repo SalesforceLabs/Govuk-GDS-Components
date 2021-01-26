d5a4ac37576220618be6cbf4ed444f058181963c
Simon Cook <simon.cook@salesforce.com>
2021-01-26 12:26:06 +0000

    renaming scripts

--- scripts/publishCommunity.sh
+++ scripts/publishCommunity.sh
@@ -2,6 +2,3 @@
 sfdx force:community:publish --name "GDS Demo"
 echo "Waiting for community to publish ..."
 sleep 20
-sfdx force:community:publish --name "GDS Landing"
-echo "Waiting for community to publish ..."
-sleep 20
