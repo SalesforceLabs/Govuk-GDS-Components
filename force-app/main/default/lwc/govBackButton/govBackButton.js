7532109411a0722ed8f56c096010b1c0e1a3814b
“Neetesh <Neetesh.jain@salesforce.com>
2021-08-17 19:45:17 +0530

    Phase 3 Check-in

---
+++ force-app/main/default/lwc/govBackButton/govBackButton.js
@@ -0,0 +1,50 @@
+import { LightningElement, api } from 'lwc';
+import { FlowNavigationBackEvent } from 'lightning/flowSupport';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class GovBackButtone extends NavigationMixin(LightningElement) {
+
+    @api availableActions = [];
+    @api backButtonLabel = '';
+    @api backAction  = false;
+    @api destinationPageName = '';
+    
+    backAvailable = false;
+
+    connectedCallback() {
+        if (this.availableActions.find(action => action === 'BACK')) {
+            this.backAvailable = true;
+        }
+    }
+
+    handleBack(event) {
+        event.preventDefault();
+        if(this.backAvailable && this.backAction) {
+            if (this.availableActions.find(action => action === 'BACK')) {
+                const navigateBackEvent = new FlowNavigationBackEvent();
+                this.dispatchEvent(navigateBackEvent);
+            }
+        } else {
+            if(this.destinationPageName) {
+                this[NavigationMixin.Navigate]({
+                    type: 'comm__namedPage',
+                    attributes: {
+                        name: this.destinationPageName
+                    },
+                    state: {
+                    }
+                });
+            }
+        }
+    }
+
+    get isVisible() {
+        if(this.backAvailable && this.backAction) {
+            return true;
+        } else if(this.destinationPageName) {
+            return true;
+        } 
+        return false;
+    }
+
+}
