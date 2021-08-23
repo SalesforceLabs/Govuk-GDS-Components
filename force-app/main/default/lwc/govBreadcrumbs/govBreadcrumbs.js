f5c92df28393d79184878b8baf1fa1513c0daf41
“Neetesh <Neetesh.jain@salesforce.com>
2021-08-23 16:24:26 +0530

    Batch 4 Ver 1.0

---
+++ force-app/main/default/lwc/govBreadcrumbs/govBreadcrumbs.js
@@ -0,0 +1,42 @@
+import { LightningElement, api, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class GovBreadcrumbs extends NavigationMixin(LightningElement) {
+
+    @api pageLabels = "";
+    @api pageAPINames = "";
+
+    @track pathItems = [];
+
+    connectedCallback() {
+        try {
+            // get the page Labels and API Names
+            let pageLabs = this.pageLabels.split(";");
+            let pageNames = this.pageAPINames.split(";");
+            
+            for (let i = 0; i < pageLabs.length; i++) {
+                this.pathItems.push({
+                    pageLabel: pageLabs[i],
+                    pageName: pageNames[i]
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        if(event.currentTarget.dataset.id && event.currentTarget.dataset.id !== '#') {
+            this[NavigationMixin.Navigate]({
+                type: 'comm__namedPage',
+                attributes: {
+                    name: event.currentTarget.dataset.id
+                },
+                state: {
+                }
+            });
+        }
+    }
+
+}
