cea9a4c501ed4e97e141a8cb6850ee0ea70ef206
“Neetesh <Neetesh.jain@salesforce.com>
2021-08-24 15:03:54 +0530

    Tab

---
+++ force-app/main/default/lwc/govTabs/govTabs.js
@@ -0,0 +1,73 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class GovTabs extends LightningElement {
+
+    @api title = "";
+    @api titleSize = "";
+    @api tabNames = "";
+    @api tabData = "";
+
+    @track data = [];
+
+    get titleClass() {
+        let titleClass;
+        switch(this.titleSize.toLowerCase()) {
+            case "small":
+                titleClass = "govuk-heading-s";
+                break;
+            case "medium":
+                titleClass = "govuk-heading-m";
+                break;
+            case "large":
+                titleClass = "govuk-heading-l";
+                break;
+            default:
+                titleClass = "govuk-heading-l";
+        }
+        return titleClass;
+    }
+
+    connectedCallback() {
+        try {
+            // get the page Labels and API Names
+            let tabs = this.tabNames.split("|");
+            let data = this.tabData.split("|");
+            
+            for (let i = 0; i < tabs.length; i++) {
+                this.data.push({
+                    heading: tabs[i],
+                    bodyText: data[i],
+                    headerClass: (i === 0) ? "govuk-tabs__list-item govuk-tabs__list-item--selected" : "govuk-tabs__list-item",
+                    bodyClass: (i === 0) ? "govuk-tabs__panel" : "govuk-tabs__panel--hidden"
+                });
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    handleTabSelection(event) {
+        this.template.querySelectorAll('.govuk-tabs__list-item--selected').forEach(element => {
+            element.classList.remove("govuk-tabs__list-item--selected");
+        });
+
+        this.template.querySelectorAll('.govuk-tabs__panel').forEach(element => {
+            element.classList.remove("govuk-tabs__panel"); 
+            element.classList.add("govuk-tabs__panel--hidden");
+        });
+          
+        this.template.querySelectorAll(`[data-id="${event.currentTarget.dataset.id}"]`).forEach(element => {
+            if(element.classList[0] === "govuk-tabs__list-item") {
+                element.classList.add("govuk-tabs__list-item--selected"); 
+            }
+        });
+    
+        this.template.querySelectorAll(`[data-id="${event.currentTarget.dataset.id}"]`).forEach(element => {
+            if(element.classList[0] === "govuk-tabs__panel--hidden") {
+                element.classList.remove("govuk-tabs__panel--hidden");
+                element.classList.add("govuk-tabs__panel"); 
+            }
+        });  
+    }
+    
+}
