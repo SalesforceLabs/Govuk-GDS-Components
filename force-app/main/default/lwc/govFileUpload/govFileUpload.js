48e47f8eba32940fe4d994f63e72d2bceb980b92
“Neetesh <Neetesh.jain@salesforce.com>
2021-08-26 16:58:28 +0530

    Batch 4

--- force-app/main/default/lwc/govFileUpload/govFileUpload.js
+++ force-app/main/default/lwc/govFileUpload/govFileUpload.js
@@ -1,14 +1,24 @@
-import { LightningElement, api, track } from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 import saveFiles from '@salesforce/apex/FileUploadController.saveFiles';
+import { MessageContext, publish, subscribe, unsubscribe } from 'lightning/messageService';
+import REGISTER_MC from '@salesforce/messageChannel/registrationMessage__c';
+import VALIDATION_MC from '@salesforce/messageChannel/validateMessage__c';
+import VALIDATION_STATE_MC from '@salesforce/messageChannel/validationStateMessage__c';
 
 export default class GovFileUpload extends LightningElement {
     
-    @api fileUploadLabel = "Upload a file";
-    @api recordId = "0019D00000HYiO8QAL";
-    @api errorMessage = "Error";    
-    @api fileNames = "";
+    @api fieldId = "";
+    @api fileUploadLabel = "";
+    @api acceptedFormats = "";
+    @api maxFileSizeInMB = 0;
+    @api required = false;
+    @api errorMessage = ""; 
+
     @api filesUploaded = []; 
 
+    @api useApexToSaveFile = false;   
+    @api recordId = "";
+    
     @track hasErrors = false;
 
     get formGroupClass() {
@@ -24,12 +34,18 @@ export default class GovFileUpload extends LightningElement {
     }
 
     handleFilesChange(event) {
+        this.clearError();
         let files = event.target.files;
         if(files.length > 0) {
             let filesName = '';
             for (let i = 0; i < files.length; i++) {
                 let file = files[i];
                 filesName = filesName + file.name + ',';
+                if(file.size > (this.maxFileSizeInMB * 1000000)) {
+                    this.hasErrors = true;
+                    this.errorMessage = `The selected file(s) must be smaller than ${this.maxFileSizeInMB} MB`;
+                    return;
+                }
                 let freader = new FileReader();
                 freader.onload = f => {
                     let base64 = 'base64,';
@@ -39,9 +55,12 @@ export default class GovFileUpload extends LightningElement {
                         Title: file.name,
                         VersionData: fileContents
                     });
-                    if(this.recordId !== "" && (i+1) === files.length) {
+                    if(this.recordId !== "" && this.useApexToSaveFile && (i+1) === files.length) {
                         this.handleSaveFiles();
-                    }   
+                    } 
+                    if((i+1) === files.length) {
+                        this.dispatchUploadEvent();
+                    }  
                 };
                 freader.readAsDataURL(file);
             }
@@ -57,8 +76,82 @@ export default class GovFileUpload extends LightningElement {
         .then(data => {
         })
         .catch(error => {
-            
+            this.hasErrors = true;
+            this.errorMessage = error;
         }); 
+    }
+
+    // messaging attributes
+    @wire(MessageContext) messageContext;
+    validateSubscription;
+
+    // LMS functions
+    subscribeMCs() {
+        if (this.validateSubscription) {
+            return;
+        }
+        this.validateSubscription = subscribe (
+            this.messageContext,
+            VALIDATION_MC, (message) => {
+                this.handleValidateMessage(message);
+            });
+    }
+
+    unsubscribeMCs() {
+        unsubscribe(this.validateSubscription);
+        this.validateSubscription = null;
+    }
+
+    connectedCallback() {
+        // subscribe to the message channels
+        this.subscribeMCs();
+
+        // publish the registration message after 0.1 sec to give other components time to initialise
+        setTimeout(() => {
+            publish(this.messageContext, REGISTER_MC, { componentId: this.uniqueFieldId });
+        }, 100);
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeMCs();
+    }
+
+    handleValidateMessage(message) {
+        this.handleValidate();
+    }
+
+    @api 
+    handleValidate() {
+        this.hasErrors = false;
+        if(this.required && this.filesUploaded.length === 0) {
+            this.hasErrors = true;
+        }
+        publish(this.messageContext, VALIDATION_STATE_MC, {
+            componentId: this.fieldId,
+            isValid: !this.hasErrors,
+            error: this.errorMessage
+        });
+        return !this.hasErrors;
+    }
+
+    @api 
+    clearError() {
+        this.hasErrors = false;
+    }
+
+    dispatchUploadEvent() {
+        // tell the flow engine about the upload
+        const attributeChangeEvent = new FlowAttributeChangeEvent('value', JSON.stringify(this.filesUploaded));
+        this.dispatchEvent(attributeChangeEvent);
+
+        // tell any parent components about the upload
+        const fileUploadEvent = new CustomEvent('fileUpload', {
+            detail: {
+                id: this.fieldId,
+                value: JSON.stringify(this.filesUploaded)
+            }
+        });
+        this.dispatchEvent(fileUploadEvent);
     }
 
 }
