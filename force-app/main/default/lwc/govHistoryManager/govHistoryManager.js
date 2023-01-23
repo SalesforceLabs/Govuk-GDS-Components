f7618d00506a5e340737b003ecf43f88b89c4285
jakubsfdc <jszelagowski@salesforce.com>
2023-01-23 10:40:28 +0000

    page title added to history component as API param

--- force-app/main/default/lwc/govHistoryManager/govHistoryManager.js
+++ force-app/main/default/lwc/govHistoryManager/govHistoryManager.js
@@ -7,12 +7,13 @@ import { LightningElement, api } from 'lwc';
 
 export default class GovHistoryManager extends LightningElement {
     @api pageName = "";
+    @api pageTitle = "";
 
     connectedCallback() {
         if(this.pageName !== undefined && this.pageName !== "") {
             const baseURL = window.location.href.split('#')[0];
             history.pushState(null, `${this.pageName}`, baseURL + `#${this.pageName.replaceAll(" ","")}`);
-            document.title = this.pageName;
+            document.title = this.pageTitle;
         }
         
     }
